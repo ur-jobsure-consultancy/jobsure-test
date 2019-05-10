c33f52599e82c81d8df9c319665bf7747db34d0c
NiceGlobals <nicefirebase@gmail.com>
2019-05-10 21:17:49 +0530

    initial commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyAnLhGevJ0nugrpdyIWSSpUu2RVydwV3Zo',
+    authDomain: 'jobsure-247d8.firebaseapp.com',
+    databaseURL: 'https://jobsure-247d8.firebaseio.com',
+    projectId: 'jobsure-247d8',
+    storageBucket: 'jobsure-247d8.appspot.com',
+    messagingSenderId: '187685039524',
+    appId: '1:187685039524:web:0f228848e7a55244'
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyBDm8RWM1eJW34tfLpxlqEkrU9zKH6Qa7k',
+    authDomain: 'jobsure-test.firebaseapp.com',
+    databaseURL: 'https://jobsure-test.firebaseio.com',
+    projectId: 'jobsure-test',
+    storageBucket: 'jobsure-test.appspot.com',
+    messagingSenderId: '913285170152',
+    appId: '1:913285170152:web:935d874dac4022c5'
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
